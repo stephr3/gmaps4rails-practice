74d5954b1a596e23d92e1f4ea1b108e4518f636c
Stephanie Gurung <stephr3@gmail.com>
2016-09-29 20:57:28 -0700

    display static map on schools#show page

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,4 +1,4 @@
 class School < ApplicationRecord
   geocoded_by :address
-  after_validation :geocode  
+  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 end
